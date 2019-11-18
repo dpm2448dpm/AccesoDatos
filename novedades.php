01e7167ba2590b280649fab7483e2c38161c0128
dpm2448dpm <dpm2448dpm@gmail.com>
2019-11-18 21:39:50 +0100

    123

--- novedades.php
+++ novedades.php
@@ -1,17 +1,23 @@
 <?php 
-echo '<div class="card text-center mt-2 bg-black border-light">
-<div class="card-header bg-info">
-  <a class="nav-link active letras-blancas" href="#"
-    ><h5>NOVEDADES</h5></a
-  >
-</div>
-<div class="card-body">
-  <h5 class="card-title letras-blancas">Gibson Sg</h5>
-  <img src="imagenes/sg1.jpg" class="card-img-top" alt="..." />
-  <p class="card-text letras-blancas">
-    Guitarra de 6 cuerdas, pastillas pasivas, madera de caoba...
-  </p>
-  
-</div>
-</div>';
+include 'conexion.php';
+$resultado = $mysqli ->query("select * from producto where novedad=true ORDER BY rand() limit 1");
+$row = $resultado -> fetch_array();
+$nombre = $row['nombre'];
+$descripcion = $row['descripcion'];
+$imagen = $row['imagen'];
 ?>
+<div class="card text-center mt-2 bg-black border-light">
+  <div class="card-header bg-info">
+    <a class="nav-link active letras-blancas" href="#">
+      <h5>NOVEDADES</h5>
+    </a>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title letras-blancas"><?php echo $nombre?></h5>
+    <img src="imagenes/<?php echo $imagen;?>" class="card-img-top" alt="..." />
+    <p class="card-text letras-blancas">
+      <?php echo $descripcion;?>
+    </p>
+
+  </div>
+</div>
